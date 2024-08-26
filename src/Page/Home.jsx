bdece30e9f077d38e541c1bcd6f804a3bd5e2c60
ThamanGurung01 <tgurung519@gmail.com>
2024-08-26 07:43:17 +0545

    inputs css updated

--- src/Page/Home.jsx
+++ src/Page/Home.jsx
@@ -32,15 +32,15 @@ const Home = () => {
     <div className='m-2  border-4 bg-gray-200 shadow-xl w-64 rounded-lg md:w-72 xl:w-96'>
     <div className='p-2 flex flex-col gap-y-3'>
       <div className='flex justify-center'>
-      <Link to="/AddNewRecipe" className='border-2 border-solid border-black px-1.5 text-center py-0.5 rounded-lg bg-black text-white shadow-lg'>Add Recipes</Link>
-      {localStorage.length>0?(<button type='button' onClick={deleteAllRecipes} className='border-2 border-solid px-1 py-0.5 ml-4 border-red-600 rounded-lg bg-red-600 text-white shadow-lg'>Delete Recipes</button>):""}
+      <Link to="/AddNewRecipe" className='btn-primary hover:btn-add-hover'>Add Recipes</Link>
+      {localStorage.length>0?(<button type='button' onClick={deleteAllRecipes} className='ml-4 btn-primary hover:btn-delete-hover'>Delete Recipes</button>):""}
       </div>
       <span className='my-2 block font-semibold text-center text-lg'>RecipeLists</span>
        {localStorage.length>0?
       <div className='flex justify-between'>
 
-      <label htmlFor="filter" className='font-semibold px-2'>Category</label>
-      <select className=' rounded-lg border-2 border-solid border-gray-100 shadow-md bg-gray-200 text-center text-base mx-2' name="filter" id="filter-recipe" onChange={(e)=>handleFilter(e)}>
+      <label htmlFor="filter" className='font-semibold px-2 '>Category</label>
+      <select className=' rounded-lg border-2 border-solid border-gray-100 shadow-md bg-gray-200 text-center text-base mx-2 hover:bg-white focus:bg-white' name="filter" id="filter-recipe" onChange={(e)=>handleFilter(e)}>
       <option value="All">All</option>
       <option value="Breakfast">Breakfast</option>
       <option value="Lunch">Lunch</option>
@@ -50,7 +50,8 @@ const Home = () => {
       </div>
       :""} 
             {(filteredRecipes.length>0)?(<ol className="list-decimal list-inside">
-      {filteredRecipes.map((element,i)=><li key={i} className='flex justify-between'><Link to={"/ViewRecipe/"+element.key} className='font-bold hover:bg-green-600 text-black px-2 py-0.5 rounded-md'>{(count++)+". "+element.RecipeName}</Link> <button type='button' onClick={()=>deleteRecipe(element.key,i)} className='border-2 border-solid border-red-600 rounded-lg px-2 bg-red-600 text-white mb-1 mx-2 shadow-sm'>Delete</button></li>)}
+      {filteredRecipes.map((element,i)=><li key={i} className='flex justify-between mb-1.5'><Link to={"/ViewRecipe/"+element.key} className='font-bold self-center ml-2
+      hover:underline hover:underline-offset-2 hover:decoration-green-600 hover:text-green-500 decoration-2'>{(count++)+". "+element.RecipeName}</Link> <button type='button' onClick={()=>deleteRecipe(element.key,i)} className='mb-1 mx-2 btn-primary hover:btn-delete-hover'>Delete</button></li>)}
       </ol>):(<span className='block text-center'>No result found</span>)}
       </div>
     </div>
